7641af6d10aafa1f3d046a87af1010a5a0779025
Kara Krzystan <kara.krzystan@gmail.com>
2021-01-13 21:53:31 -0700

    Create routes to serve index.html

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -97,6 +98,10 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
